a66a29dccfd9fb89377ad392a3d41731df7d9773
AbdalrhmanBanyissa <aboood.banyissa.94@gmail.com>
2021-06-15 15:54:27 +0300

    modifing lab 07

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-const express = require('express'); // require the express package
+const express = require("express"); // require the express package
 const app = express(); // initialize your express app instance
- const weatherData = require('./data/weather.json');
- require('dotenv').config();
- const cors = require('cors');
+const weatherData = require("./data/weather.json");
+require("dotenv").config();
+const cors = require("cors");
 
- const PORT = process.env.PORT;
+const PORT = process.env.PORT;
 
-app.use(cors()) // after you initialize your express app instance
-// a server endpoint 
+app.use(cors()); // after you initialize your express app instance
+// a server endpoint
 
-app.get('/weather',(req,res)=>{
-  res.json(weatherData)
-})
- 
+app.get("/weather", (req, res) => {
+  let weatherArray = weatherData.data.map((item) => new Forcast(item));
+  res.send(weatherArray);
+});
+
+class Forcast {
+  constructor(item) {
+    this.date = item.valid_date;
+    this.weather = item.weather.description;
+  }
+}
+
 app.listen(PORT, () => {
-  console.log(`Server Started on ${PORT}`)
-})
+  console.log(`Server Started on ${PORT}`);
+});

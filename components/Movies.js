a27564abe4a7f1b2b75793434733a726250afe74
AbdalrhmanBanyissa <aboood.banyissa.94@gmail.com>
2021-06-19 13:48:55 +0300

    lab 10 is completed

--- components/Movies.js
+++ components/Movies.js
@@ -2,20 +2,31 @@
 
 const axios = require("axios");
 const Movies = require("./Movie");
+let moviesCacheObj = {};
 
 const movieHandler = (req, res) => {
-    const MOVIE_KEY = process.env.MOVIE_KEY;
-    const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_KEY}&query=${req.query.searchQuery}`;
+  const MOVIE_KEY = process.env.MOVIE_KEY;
+  const city = req.query.searchQuery;
+
+  if (moviesCacheObj[city] !== undefined) {
+    console.log("get the movies data from the cache object");
+    res.send(moviesCacheObj[city]); // get the data from the cache object
+  } else {
+    const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_KEY}&query=${city}`;
     axios
       .get(movieUrl)
       .then((responce) => {
-        const moviearray = responce.data.results.map(movieItem => new Movies (movieItem));
+        const moviearray = responce.data.results.map(
+          (movieItem) => new Movies(movieItem)
+        );
+        moviesCacheObj[city] = moviearray; // save the data in the cache object
+        console.log("get the movies data from the api");
         res.send(moviearray);
       })
-      .catch(error => {
-        res.send("Error 404! "+error)
+      .catch((error) => {
+        res.send("Error 404! " + error);
       });
   }
+};
 
-  
-  module.exports = movieHandler;
+module.exports = movieHandler;
